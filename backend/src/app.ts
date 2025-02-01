2b3f4d1df963c16e3d84c662bfb8ab0b03a2be14
muhammadderic <muhammad.donny.e@gmail.com>
2025-02-02 01:11:11 +0700

    add cors in backend and create override docker-compose file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,21 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
 import authRoutes from "./modules/user/presentation/auth.routes";
 import { errorHandler } from "./common/middleware/error-handler";
 
+dotenv.config();
+
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
